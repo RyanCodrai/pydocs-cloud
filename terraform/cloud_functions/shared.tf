04e82b0e9b13991d21c386487588d0fa4dd3b625
Ryan Codrai <ryan.codrai@gmail.com>
2025-10-19 16:12:15 -0700

    Ensure appropriate service account permissions

--- terraform/cloud_functions/shared.tf
+++ terraform/cloud_functions/shared.tf
@@ -13,18 +13,31 @@ resource "google_storage_bucket" "function_source" {
   }
 }
 
-# Get current project configuration from provider
-data "google_client_config" "current" {
-}
+# Note: Eventarc service agent is automatically created and granted permissions
+# by Google when you first create an Eventarc trigger. No manual IAM binding needed.
 
-# Get project data to retrieve project number
+# Get project number for Cloud Build service account
 data "google_project" "project" {
-  project_id = data.google_client_config.current.project
+  project_id = var.project_id
 }
 
-# Grant Eventarc service agent the ability to manage events
-resource "google_project_iam_member" "eventarc_service_agent" {
-  project = data.google_client_config.current.project
-  role    = "roles/eventarc.serviceAgent"
-  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-eventarc.iam.gserviceaccount.com"
+# Grant Cloud Build permissions to the default Compute Service Account
+# Required for Cloud Functions Gen 2 in projects created after July 2024
+resource "google_project_iam_member" "cloudbuild_sa" {
+  project = var.project_id
+  role    = "roles/cloudbuild.builds.builder"
+  member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
+}
+
+# Get GCS service account (this triggers its creation if it doesn't exist)
+data "google_storage_project_service_account" "gcs_account" {
+  project = var.project_id
+}
+
+# Grant Cloud Storage service account Pub/Sub Publisher role for Eventarc triggers
+# Required for GCS CloudEvent triggers to publish to Pub/Sub
+resource "google_project_iam_member" "gcs_pubsub_publishing" {
+  project = var.project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
 }
