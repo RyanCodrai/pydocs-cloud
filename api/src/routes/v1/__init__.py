2c49043a50f72027416fec973772a6bb03ad86a1
Ryan Codrai <ryan.codrai@gmail.com>
2025-10-20 13:13:25 -0700

    Exclude api key router and user router for now

--- api/src/routes/v1/__init__.py
+++ api/src/routes/v1/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from src.routes.v1.apikeys.router import router as api_key_router
-from src.routes.v1.users.router import router as user_router
+# from src.routes.v1.apikeys.router import router as api_key_router
+# from src.routes.v1.users.router import router as user_router
 
 router = APIRouter(prefix="/api/v1")
-router.include_router(user_router)
-router.include_router(api_key_router)
+# router.include_router(user_router)
+# router.include_router(api_key_router)
