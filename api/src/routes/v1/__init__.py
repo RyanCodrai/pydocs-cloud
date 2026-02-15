050de34c4b5b005371ca6884b71d281f38251b49
Ryan Codrai <ryan.codrai@gmail.com>
2026-02-15 18:31:04 +0000

    Add conditional imports for user based routes

--- api/src/routes/v1/__init__.py
+++ api/src/routes/v1/__init__.py
@@ -12,6 +12,13 @@ def create_router() -> APIRouter:
         router.include_router(webhooks_router)
         router.include_router(lookup_router)
 
+    if settings.SERVICE_TYPE in {"all", "user"}:
+        from src.routes.v1.apikeys.router import router as apikeys_router
+        from src.routes.v1.users.router import router as users_router
+
+        router.include_router(users_router)
+        router.include_router(apikeys_router)
+
     return router
 
 
