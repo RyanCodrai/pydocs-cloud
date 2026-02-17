08088f39b226f5508b1359434344a14dde311dc3
Ryan Codrai <ryan.codrai@gmail.com>
2026-02-17 17:35:48 +0000

    Remove lookup endpoint in user api

--- api/src/routes/v1/__init__.py
+++ api/src/routes/v1/__init__.py
@@ -13,13 +13,11 @@ def create_router() -> APIRouter:
     if settings.SERVICE_TYPE in {"all", "user"}:
         from src.routes.v1.apikeys.router import router as apikeys_router
         from src.routes.v1.auth.router import router as auth_router
-        from src.routes.v1.lookup.router import router as lookup_router
         from src.routes.v1.users.router import router as users_router
 
         router.include_router(users_router)
         router.include_router(apikeys_router)
         router.include_router(auth_router)
-        router.include_router(lookup_router)
 
     return router
 
